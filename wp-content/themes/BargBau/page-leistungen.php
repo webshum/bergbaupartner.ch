a0be1f7327e492d4649c146386a5e05084be5a57
WebShum <webshumweb@gmail.com>
2023-11-15 13:54:15 +0200

    Fix icon

--- wp-content/themes/BargBau/page-leistungen.php
+++ wp-content/themes/BargBau/page-leistungen.php
@@ -18,7 +18,7 @@ get_header();
 
 		<?php if (!isArrayEmpty($images_row)) : ?>
 			<div class="image-row">
-				<?php $icon = [1,3,'bottle']; $count = 0; ?>
+				<?php $icon = [1,2,'bottle']; $count = 0; ?>
 				<?php foreach ($images_row as $section): ?>
 					<?php if (!empty($section['link'])) : ?>
 						<a href="<?= $section['link']['url']; ?>" <?= (!empty($section['link']['target'])) ? 'target="_blank"' : ''; ?> class="section hover-image">
